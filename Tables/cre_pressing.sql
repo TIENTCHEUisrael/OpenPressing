3f8b4f8edff9b6df26840161f869d93d3db94473
LynlynDev <101403594+LynlynDev@users.noreply.github.com>
2022-11-19 11:50:25 +0100

    Update cre_pressing.sql

--- Tables/cre_pressing.sql
+++ Tables/cre_pressing.sql
@@ -1,1 +1,23 @@
-PROMPT ******************* CREATE PReSSING TABLES *****************
+PROMPT ******************* CREATE PRESSING TABLES *****************
+
+CREATE IF NOT EXISTS TABLE PRESSINGS 
+(
+   ID_PRESSING          NUMBER(10) ,
+   ID_USER              char(10),
+   NOM                  varchar(255), 
+   VILLE                varchar(255),
+   OUVERTURE            timestamp ,
+   FERMETURE            timestamp   
+);
+
+alter table PRESSINGS
+  ADD (constraint PRESSING_pk 
+      PRIMARY KEY  (ID_PRESSING)
+ );
+
+alter table PRESSINGS
+   ADD constraint FK_PRESSING_REFERENCE_UTILISAT 
+        FOREIGN KEY (ID_USER)
+              REFERENCES UTILISATEUR (ID_USER);
+
+
