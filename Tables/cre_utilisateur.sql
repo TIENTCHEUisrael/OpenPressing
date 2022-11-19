fe306a1205829c316828d5c149f6a28e8033e59b
BriceNogue <93254397+BriceNogue@users.noreply.github.com>
2022-11-19 19:00:27 +0100

    Create table user.

--- Tables/cre_utilisateur.sql
+++ Tables/cre_utilisateur.sql
@@ -1,1 +1,8 @@
 PROMPT ******************* CREATE UTILISATEUR TABLES *****************
+
+CREATE IF NOT EXISTS TABLE UTILISATEUR
+(
+    ID_USER              char(10),                     not null,
+    ID_PERSPHY           numeric(10) 
+);
+
