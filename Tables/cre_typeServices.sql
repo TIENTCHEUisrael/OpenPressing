a615f7deb5fdbf00781d65a3872063a2ee9eaf80
BriceNogue <93254397+BriceNogue@users.noreply.github.com>
2022-11-19 19:21:31 +0100

    Create table TYPESERVICE an add constrainst.

--- Tables/cre_typeServices.sql
+++ Tables/cre_typeServices.sql
@@ -1,1 +1,14 @@
 PROMPT ******************* CREATE TYPESERVICES TABLES *****************
+
+CREATE IF NOT EXISTS TABLE TYPESERVICES
+(
+    ID_TYPESERVICE       number(10),                   not null,
+    NOM                  varchar(255)  
+);
+
+ALTER TABLE TYPESERVICES
+ADD
+(
+    CONSTRAINT TYPESERVICES_PK
+    PRIMARY KEY (ID_TYPESERVICE)
+);
