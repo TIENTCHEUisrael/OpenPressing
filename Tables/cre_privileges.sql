4318d12423a8123f3a0c928b2f351eea0a5f5777
LynlynDev <101403594+LynlynDev@users.noreply.github.com>
2022-11-19 11:56:25 +0100

    Update cre_privileges.sql

--- Tables/cre_privileges.sql
+++ Tables/cre_privileges.sql
@@ -1,1 +1,15 @@
 PROMPT ******************* CREATE PRIVILEGES TABLES *****************
+
+CREATE IF NOT EXISTS TABLE PRIVILEGES 
+(
+   ID_PRIVILEGE         NUMBER(10),
+   DESCRIPTION          VARCHAR(255)                   
+);
+
+alter table PRIVILEGES
+  ADD (constraint PRESSING_pk 
+      PRIMARY KEY  (ID_PRIVILEGES)
+ );
+
+
+
