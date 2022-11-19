dd8021a81eb03a068f50365e4b754c658ce39898
LynlynDev <101403594+LynlynDev@users.noreply.github.com>
2022-11-19 19:42:55 +0100

    Update cre_promo.sql

--- Tables/cre_promo.sql
+++ Tables/cre_promo.sql
@@ -1,1 +1,10 @@
 PROMPT ******************* CREATE PROMO TABLES *****************
+create table PROMO 
+(
+   ID_ANNOCES          Number(10),
+   CODEPROMO           VARCHAR(255) 
+);
+
+alter table PROMO
+   add constraint FK_PROMO_REFERENCE_ANNONCES foreign key (ID_ANNOCES)
+      references ANNONCES (ID_ANNOCES);
