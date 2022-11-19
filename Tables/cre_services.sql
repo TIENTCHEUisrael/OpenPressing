2058159403d4ec2d89011f3588ed056ad611f298
BriceNogue <93254397+BriceNogue@users.noreply.github.com>
2022-11-19 20:05:15 +0100

    CREATE TABLE SERVICE.

--- Tables/cre_services.sql
+++ Tables/cre_services.sql
@@ -1,1 +1,9 @@
 PROMPT ******************* CREATE SERVICES TABLES *****************
+
+CREATE IF NOT EXISTS TABLE SERVICES
+(
+    ID_SERVICES          number(10)                    not null,
+    ID_TYPESERVICE       number(10)                    null,
+    NOM                  varchar(255)  
+);
+
