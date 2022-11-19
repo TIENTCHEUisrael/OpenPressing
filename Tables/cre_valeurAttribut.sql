89e4e5b1734f5bb92823269a4339b1a96e455c93
BriceNogue <93254397+BriceNogue@users.noreply.github.com>
2022-11-19 20:25:43 +0100

    Create ValeurAttributs.

--- Tables/cre_valeurAttribut.sql
+++ Tables/cre_valeurAttribut.sql
@@ -1,2 +1,7 @@
 PROMPT ***************** CREATE VALEUR ATTRIBUT TABLE **************************
 
+CREATE IF NOT EXISTS TABLE VALEURATTRIBUTS
+(
+    ID_ATTRIBUT          NUMBER(10),                  null,
+    VALEUR               varchar(255)
+);
