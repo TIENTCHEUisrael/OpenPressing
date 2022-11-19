23c0ef69f388b89d22df3cb3b8b62e8282fa24a1
BriceNogue <93254397+BriceNogue@users.noreply.github.com>
2022-11-19 19:32:14 +0100

    CREATE TABLE TYPELINGE.

--- Tables/cre_typeLinge.sql
+++ Tables/cre_typeLinge.sql
@@ -1,1 +1,8 @@
 PROMPT ******************* CREATE TYPELINGE TABLES *****************
+
+CREATE IF NOT EXISTS TABLE TYPELINGE
+(
+    ID_TYPELINGE         number(10),                   not null,
+    NOM                  varchar(255)  
+);
+
